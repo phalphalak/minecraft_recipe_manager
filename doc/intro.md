ed35fc2b7d587908e83a0c9d1d1807eba823b2dc
phalphalak@gmail.com <phalphalak@gmail.com>
2013-05-17 23:10:48 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to minecraft_recipe_manager
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/minecraft_recipe_manager/core_test.clj
@@ -0,0 +1,7 @@
+(ns minecraft-recipe-manager.core-test
+  (:use clojure.test
+        minecraft-recipe-manager.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
